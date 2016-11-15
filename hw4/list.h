214f62563e6d7e70422c2a7e5cacf455ce0e1d60
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2016-11-15 13:42:21 -0500

    Added files to modify in lab4

--- hw4/list.h
+++ hw4/list.h
@@ -7,85 +7,78 @@
 template<class Ele, class Keytype> class list;
 
 template<class Ele, class Keytype> class list {
- private:
+private:
   Ele *my_head;
   unsigned long long my_num_ele;
- public:
-  list(){
+public:
+
+  list() {
     my_head = NULL;
     my_num_ele = 0;
   }
 
   void setup();
 
-  unsigned num_ele(){return my_num_ele;}
+  unsigned num_ele() {
+    return my_num_ele;
+  }
 
-  Ele *head(){ return my_head; }
+  Ele *head() {
+    return my_head;
+  }
   Ele *lookup(Keytype the_key);
 
   void push(Ele *e);
   Ele *pop();
-  void print(FILE *f=stdout);
+  void print(FILE *f = stdout);
 
   void cleanup();
 };
 
-template<class Ele, class Keytype> 
-void 
-list<Ele,Keytype>::setup(){
+template<class Ele, class Keytype> void list<Ele, Keytype>::setup() {
   my_head = NULL;
   my_num_ele = 0;
 }
 
-template<class Ele, class Keytype> 
-void 
-list<Ele,Keytype>::push(Ele *e){
+template<class Ele, class Keytype> void list<Ele, Keytype>::push(Ele *e) {
   e->next = my_head;
   my_head = e;
   my_num_ele++;
 }
 
-template<class Ele, class Keytype> 
-Ele *
-list<Ele,Keytype>::pop(){
+template<class Ele, class Keytype> Ele *list<Ele, Keytype>::pop() {
   Ele *e;
   e = my_head;
-  if (e){
+  if (e) {
     my_head = e->next;
     my_num_ele--;
   }
   return e;
 }
 
-template<class Ele, class Keytype> 
-void 
-list<Ele,Keytype>::print(FILE *f){
+template<class Ele, class Keytype> void list<Ele, Keytype>::print(FILE *f) {
   Ele *e_tmp = my_head;
 
-  while (e_tmp){
+  while (e_tmp) {
     e_tmp->print(f);
     e_tmp = e_tmp->next;
   }
 }
 
-template<class Ele, class Keytype> 
-Ele *
-list<Ele,Keytype>::lookup(Keytype the_key){
+template<class Ele, class Keytype> Ele * list<Ele, Keytype>::lookup(Keytype the_key) {
   Ele *e_tmp = my_head;
-  
-  while (e_tmp && (e_tmp->key() != the_key)){
+
+  while (e_tmp && (e_tmp->key() != the_key)) {
     e_tmp = e_tmp->next;
   }
   return e_tmp;
 }
 
-template<class Ele, class Keytype> 
-void
-list<Ele,Keytype>::cleanup(){
+template<class Ele, class Keytype> void list<Ele, Keytype>::cleanup() {
   Ele *e_tmp = my_head;
   Ele *e_next;
 
-  while (e_tmp){
+  while (e_tmp) {
     e_next = e_tmp->next;
     delete e_tmp;
     e_tmp = e_next;
