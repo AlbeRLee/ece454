755aefca555bfe8279300efb6c4ce9aa33abf5b3
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2016-11-22 22:49:03 -0500

    reformatting

--- hw5/hw5src/life.c
+++ hw5/hw5src/life.c
@@ -13,11 +13,10 @@
  * Game of life implementation
  ****************************************************************************/
 char*
-game_of_life (char* outboard, 
-	      char* inboard,
-	      const int nrows,
-	      const int ncols,
-	      const int gens_max)
-{
-  return sequential_game_of_life (outboard, inboard, nrows, ncols, gens_max);
+game_of_life(char* outboard,
+  char* inboard,
+  const int nrows,
+  const int ncols,
+  const int gens_max) {
+  return sequential_game_of_life(outboard, inboard, nrows, ncols, gens_max);
 }
--- hw5/hw5src/util.h
+++ hw5/hw5src/util.h
@@ -8,21 +8,25 @@
  * as x and m are both positive.  This is helpful for computing
  * toroidal boundary conditions.
  */
-static inline int 
-mod (int x, int m)
-{
+static inline int
+mod(int x, int m) {
   return (x < 0) ? ((x % m) + m) : (x % m);
+  //  	return (x >= 0) ? (x % m) : ((x % m) + m);
+  //	//int res = x % m;
+  //  	//return (res > 0) ? res : (res + m);
 }
 
 /**
  * Given neighbor count and current state, return zero if cell will be
  * dead, or nonzero if cell will be alive, in the next round.
  */
-static inline char 
-alivep (char count, char state)
-{
-  return (! state && (count == (char) 3)) ||
-    (state && (count >= 2) && (count <= 3));
+static inline char
+alivep(char count, char state) {
+  return (!state && (count == (char) 3)) ||
+          (state && (count >= 2) && (count <= 3));
+  //  // if state == 0 and count == 3, return 1; if state!=0 and (count==2 or count ==3), return 1
+  //	return (state == 0) ? (count == 3) : (count == 2 || count == 3);
+  //  	//return (state && count >= 2 && count <= 3) || (!state && (count == (char) 3));
 }
 
 #endif /* _util_h */
