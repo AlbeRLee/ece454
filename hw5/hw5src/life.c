04591b193fec9d3beb5416192aa32bbc90e6c50c
Gligor Djogo <g.djogo@mail.utoronto.ca>
2016-12-05 15:35:31 -0500

    Barrier between generations ~4.6X

--- hw5/hw5src/life.c
+++ hw5/hw5src/life.c
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
 
 /* 
  * Swapping the two boards only involves swapping pointers, not copying values.
@@ -30,7 +29,7 @@
  * Parallelization
  ****************************************************************************/
 
-#define NUM_THREADS 4
+#define NUM_THREADS 8
 
 typedef struct thread_arguments {
   char* outboard;
@@ -40,6 +39,8 @@ typedef struct thread_arguments {
   int LDA;
   int start_row;
   int end_row;
+  int gens_max;
+  pthread_barrier_t* gens_barrier;
 } thread_arg;
 
 /*****************************************************************************
@@ -47,10 +48,10 @@ typedef struct thread_arguments {
  ****************************************************************************/
 
 void*
-thread_worker(void* args_){
+thread_worker(void* args_) {
   // get thread arguments with the row boundaries
-  thread_arg* arg = (thread_arg*)args_;
-  
+  thread_arg* arg = (thread_arg*) args_;
+
   char* outboard = arg->outboard;
   char* inboard = arg->inboard;
   const int nrows = arg->nrows;
@@ -59,53 +60,62 @@ thread_worker(void* args_){
   const int LDA = arg->LDA;
   const int start_col = arg->start_row;
   const int end_col = arg->end_row;
-  
-  int i, j;
+  const int gens_max = arg->gens_max;
+  pthread_barrier_t* next_gen = arg->gens_barrier;
+
+  int curgen, i, j;
   char destiny;
   char tbc, tbs, tbn, n, c, s;
   int jwest, jeast, nrows_1, nrows_2; // jwest; //jeast;
   nrows_1 = nrows - 1;
   nrows_2 = nrows - 2;
-  
-  // swap the iterations to have rows inside loop and cols outside
-  for (j = start_col; j < end_col; j++) {
-    
-    // why were these const int?
-    jwest = mod(j - 1, ncols);
-    //inorth = (i-1 < 0) ? (nrows-1) : (i-1);
-    jeast = mod(j + 1, ncols);
-    //isouth = (i+1 == nrows) ? (0) : (i+1);
-    
-    // assign the values one step ahead
-    n = BOARD(inboard, nrows_2, j);
-    c = BOARD(inboard, nrows_1, j);
-    s = BOARD(inboard, 0, j);
 
-    tbn = BOARD(inboard, nrows_2, jwest) + BOARD(inboard, nrows_2, jeast);
-    tbc = BOARD(inboard, nrows_1, jwest) + BOARD(inboard, nrows_1, jeast);
-    tbs = BOARD(inboard, 0, jwest) + BOARD(inboard, 0, jeast);
+  for (curgen = 0; curgen < gens_max; curgen++) {
 
-    const char neighborCnt = n + s + tbc + tbn + tbs;
-    destiny = alivep(neighborCnt, c);
-    //if (destiny != c)
-    BOARD(outboard, nrows_1, j) = destiny;
+    // swap the iterations to have rows inside loop and cols outside
+    for (j = start_col; j < end_col; j++) {
 
-    for (i = 1; i < nrows; i++) // will write up to nrows-1
-    {
-      // sliding window tactic, cut down the #of mem reads,
-      // only read on the leading edge of 3x3 game box front
-      tbn = tbc;
-      tbc = tbs;
-      n = c;
-      c = s;
-      tbs = BOARD(inboard, i, jwest) + BOARD(inboard, i, jeast);
-      s = BOARD(inboard, i, j);
+      // why were these const int?
+      jwest = mod(j - 1, ncols);
+      //inorth = (i-1 < 0) ? (nrows-1) : (i-1);
+      jeast = mod(j + 1, ncols);
+      //isouth = (i+1 == nrows) ? (0) : (i+1);
 
-      const char neighbor_count = n + s + tbc + tbn + tbs;
-      destiny = alivep(neighbor_count, c);
-      BOARD(outboard, i-1, j) = destiny;
+      // assign the values one step ahead
+      n = BOARD(inboard, nrows_2, j);
+      c = BOARD(inboard, nrows_1, j);
+      s = BOARD(inboard, 0, j);
+
+      tbn = BOARD(inboard, nrows_2, jwest) + BOARD(inboard, nrows_2, jeast);
+      tbc = BOARD(inboard, nrows_1, jwest) + BOARD(inboard, nrows_1, jeast);
+      tbs = BOARD(inboard, 0, jwest) + BOARD(inboard, 0, jeast);
+
+      const char neighborCnt = n + s + tbc + tbn + tbs;
+      destiny = alivep(neighborCnt, c);
+      //if (destiny != c)
+      BOARD(outboard, nrows_1, j) = destiny;
+
+      for (i = 1; i < nrows; i++) // will write up to nrows-1
+      {
+        // sliding window tactic, cut down the #of mem reads,
+        // only read on the leading edge of 3x3 game box front
+        tbn = tbc;
+        tbc = tbs;
+        n = c;
+        c = s;
+        tbs = BOARD(inboard, i, jwest) + BOARD(inboard, i, jeast);
+        s = BOARD(inboard, i, j);
+
+        const char neighbor_count = n + s + tbc + tbn + tbs;
+        destiny = alivep(neighbor_count, c);
+        BOARD(outboard, i - 1, j) = destiny;
+      }
+
     }
 
+    SWAP_BOARDS(outboard, inboard);
+    
+    pthread_barrier_wait(next_gen);
   }
   
   pthread_exit(NULL);
@@ -113,50 +123,53 @@ thread_worker(void* args_){
 
 char*
 parallel_game_of_life(char* outboard,
-        char* inboard,
-        const int nrows,
-        const int ncols,
-        const int gens_max) {
-  
+    char* inboard,
+    const int nrows,
+    const int ncols,
+    const int gens_max) {
+
   // HINT: in the parallel decomposition, LDA may not be equal to nrows!!!
   //const int LDA = nrows;
-  int curgen, i, i_rows;
+  int i, i_rows;
   const int rows_per_thread = nrows/NUM_THREADS; //except last thread gets extra
-  
+
   pthread_t* threads = malloc(sizeof(pthread_t) * NUM_THREADS);
+  //  pthread_t threads[NUM_THREADS];
   thread_arg* thread_args = malloc(sizeof(thread_arg) * NUM_THREADS);
-//  pthread_barrier_t next_gen;
-//  pthread_barrier_init(next_gen, 0, NUM_THREADS);
+  //  thread_arg thread_args[NUM_THREADS];
+  pthread_barrier_t* gens_barrier = malloc(sizeof(pthread_barrier_t));
+  //  pthread_barrier_t gens_barrier;
+  pthread_barrier_init(gens_barrier, 0, NUM_THREADS);
   
   i_rows = 0;
   for (i = 0; i < NUM_THREADS; i++) {
-      thread_args[i].nrows = nrows;
-      thread_args[i].ncols = ncols;
-      thread_args[i].LDA = nrows;
-      thread_args[i].start_row = i_rows;
-      i_rows += rows_per_thread;
-      thread_args[i].end_row = i_rows;
+    thread_args[i].nrows = nrows;
+    thread_args[i].ncols = ncols;
+    thread_args[i].LDA = nrows;
+    thread_args[i].start_row = i_rows;
+    i_rows += rows_per_thread;
+    thread_args[i].end_row = i_rows;
+    thread_args[i].gens_max = gens_max;
+    thread_args[i].gens_barrier = gens_barrier;
   }
-
-  for (curgen = 0; curgen < gens_max; curgen++) {
-    
-    for (i = 0; i < NUM_THREADS; i++) {
-      thread_args[i].inboard = inboard;
-      thread_args[i].outboard = outboard;
-      pthread_create(&threads[i], NULL, thread_worker, (void*) &thread_args[i]);
-    }
-    /* Compute one generation of the game-of-life grid */
-    for (i = 0; i < NUM_THREADS; i++) {
-      pthread_join(threads[i], NULL);
-    }
-    
-    SWAP_BOARDS(outboard, inboard);
+  
+  for (i = 0; i < NUM_THREADS; i++) {
+    thread_args[i].inboard = inboard;
+    thread_args[i].outboard = outboard;
+    pthread_create(&threads[i], NULL, thread_worker, (void*) &thread_args[i]);
+  }
+  /*
+   * Compute all generations of the game-of-life grid
+   */
+  for (i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], NULL);
   }
   
   // clean up dynamic mem structures
   free(threads);
   free(thread_args);
-  
+  free(gens_barrier);
+
   /* 
    * We return the output board, so that we know which one contains
    * the final result (because we've been swapping boards around).
@@ -196,7 +209,7 @@ game_of_life(char* outboard,
    * - do not have to parallelize for less than 32x32
    */
 
-  if ((nrows < 32) || (nrows != ncols) ||(nrows > 10000)) // || (nrows % 4)) (nrows & !0x4)
+  if ((nrows < 32) || (nrows != ncols) || (nrows > 10000)) // || (nrows % 4)) (nrows & !0x4)
     return sequential_game_of_life(outboard, inboard, nrows, ncols, gens_max);
   else
     return parallel_game_of_life(outboard, inboard, nrows, ncols, gens_max);
